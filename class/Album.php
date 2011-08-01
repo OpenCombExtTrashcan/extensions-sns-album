850f7dcccd790e292a2f078cd3568daafccc5bf3
alee <zq@alee2002.com>
2011-08-01 14:04:04 +0800

    add class Album

---
+++ class/Album.php
@@ -0,0 +1,27 @@
+<?php
+namespace oc\ext\album ;
+
+use jc\mvc\model\db\orm\PrototypeAssociationMap;
+use oc\ext\Extension;
+
+class Album extends Extension
+{
+	public function load()
+	{
+		// 定义ORM
+        $this->defineOrm(PrototypeAssociationMap::singleton()) ;
+	}
+	
+	
+	/**
+	 * 
+	 * Enter description here ...
+	 * @param PrototypeAssociationMap $aAssocMap
+	 */
+	public function defineOrm(PrototypeAssociationMap $aAssocMap)
+	{
+	}
+	
+}
+
+?><? ?>
